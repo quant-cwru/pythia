fc86a4508f6684522b26e7667ab9d8975fd501ac
Andrew Ye <aly37@case.edu>
2024-03-19 00:51:03 -0400

    create yfinance dataset

---
+++ YFinDataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from dataset import Dataset
+
+class YFinDataset(Dataset):
+    def __init__(self):
+        pass
+    def __getitem__(self):
+        pass
+
+dataset = YFinDataset()
--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 """
 Dataset class
 """
+from abc import ABC, abstractmethod
 
-class Dataset():
+class Dataset(ABC):
+    @abstractmethod
     def __getitem__(self, index):
         raise NotImplementedError("Subclasses should implement __getitem__.")
